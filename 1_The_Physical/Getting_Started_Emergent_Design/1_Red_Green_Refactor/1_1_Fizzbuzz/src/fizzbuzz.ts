68dfe4d845f95343f5d3c32a8201b4e83f08f9bf
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 21:09:59 +0000

    refactor: transform the fizzbuzz function to validate both multiples of three and multiples of five values

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export default function fizzbuzz(value: number): string {
+  if (value % 3 === 0 && value % 5 === 0) return "FizzBuzz";
   if (value % 3 === 0) return "Fizz";
   if (value % 5 === 0) return "Buzz";
 
