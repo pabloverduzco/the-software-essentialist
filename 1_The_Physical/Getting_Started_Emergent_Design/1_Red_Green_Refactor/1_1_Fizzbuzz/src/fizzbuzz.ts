e6cdb8861146acc58ac2c7e2f4998a70ee62a6fb
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 21:35:12 +0000

    refactor: transform the fizzbuzz function to catch values out of the valid range

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,8 @@
 export default function fizzbuzz(value: number): string {
+  if (value < 1) {
+    throw new Error("value is out of the valid range");
+  }
+
   if (value % 3 === 0 && value % 5 === 0) return "FizzBuzz";
   if (value % 3 === 0) return "Fizz";
   if (value % 5 === 0) return "Buzz";
