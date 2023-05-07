718a1bca320c8adaf44ae84f7a2b657efa5ee14b
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 22:59:33 +0000

    test: refactor test to prevent duplication

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,25 +1,40 @@
 import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("should return 'Fizz' when 3 is used as multiple of three value", () => {
-    expect(fizzbuzz(3)).toBe("Fizz");
+  describe("given a multiple of three", () => {
+    it.each([3, 9, 42])(
+      "should return 'Fizz' when %d is used as value",
+      (value) => {
+        expect(fizzbuzz(value)).toBe("Fizz");
+      }
+    );
   });
 
-  it("should return 'Buzz' when 5 is used as multiple of five value", () => {
-    expect(fizzbuzz(5)).toBe("Buzz");
+  describe("given a multiple of five", () => {
+    it.each([5, 20])(
+      "should return 'Buzz' when %d is used as value",
+      (value) => {
+        expect(fizzbuzz(value)).toBe("Buzz");
+      }
+    );
   });
 
-  it("should return 'FizzBuzz' when a value is both multiple of three and multiple of three", () => {
-    expect(fizzbuzz(30)).toBe("FizzBuzz");
+  describe("given a multiple of three and five", () => {
+    it.each([15, 45])(
+      "should return 'FizzBuzz' when %d is used as value",
+      (value) => {
+        expect(fizzbuzz(value)).toBe("FizzBuzz");
+      }
+    );
   });
 
   it("should return '1' as string since the number isn't a multiple of three or five", () => {
     expect(fizzbuzz(1)).toBe("1");
   });
 
-  it("should throw an error when values is 0 since the minimum allowed value is 1", () => {
+  it("should throw an error when values is -12 since the minimum allowed value is 1", () => {
     expect(() => {
-      fizzbuzz(0);
+      fizzbuzz(-12);
     }).toThrow(Error);
   });
 
