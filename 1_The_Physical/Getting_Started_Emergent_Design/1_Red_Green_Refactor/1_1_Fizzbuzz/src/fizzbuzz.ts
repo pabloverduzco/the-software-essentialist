e1ebd404822d45550e8df9d08efd6a3dbd5072c3
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 21:17:04 +0000

    refactor: refactor fizzbuzz function to return the value a string

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,5 +3,5 @@ export default function fizzbuzz(value: number): string {
   if (value % 3 === 0) return "Fizz";
   if (value % 5 === 0) return "Buzz";
 
-  return "";
+  return value.toString(10);
 }
