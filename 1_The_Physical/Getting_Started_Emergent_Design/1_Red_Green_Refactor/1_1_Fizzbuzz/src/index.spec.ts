c796eaeda82cb594f0745b831a13f47c6a9964f5
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 20:47:12 +0000

    test: create a test to validate multiple of three values

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,6 +1,10 @@
 import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
+  it("should return 'Fizz' when 3 is uses as multiple of three value", () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+  });
+
   it("should return the input as string", () => {
     expect(typeof fizzbuzz(1)).toBe("string");
   });
