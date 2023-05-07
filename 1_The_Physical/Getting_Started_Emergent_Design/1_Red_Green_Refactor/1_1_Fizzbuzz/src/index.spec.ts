38cd02fed2b1e2fceab510d18df102232f0e5a9b
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-07 20:53:20 +0000

    test: create a test to validate values as multiples of five

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,8 +1,12 @@
 import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("should return 'Fizz' when 3 is uses as multiple of three value", () => {
+  it("should return 'Fizz' when 3 is used as multiple of three value", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
+  });
+
+  it("should return 'Buzz' when 5 is used as multiple of five value", () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
   });
 
   it("should return the input as string", () => {
