305793eda04cd4612cd50610c5b42701dc24c347
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-08 03:44:37 +0000

    refactor(test): use a global palindromeChecker variable to prevent duplication

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,18 +1,21 @@
 import PalindromeChecker from ".";
 
+let palindromeChecker: PalindromeChecker;
+
+beforeEach(() => {
+  palindromeChecker = new PalindromeChecker();
+});
+
 describe("palindrome checker", () => {
   it("should return 'true' when 'mom' since 'mom' is a palindrome", () => {
-    let palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("mom")).toBe(true);
   });
 
   it("should return 'false' when 'bill' is the input since this is not a palindrome", () => {
-    let palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("bill")).toBe(false);
   });
 
   it("should return 'true' when 'Mom' is the input since the word is a palindrome and casing should be ignores", () => {
-    let palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.isPalindrome("Mom")).toBe(true);
   });
 });
