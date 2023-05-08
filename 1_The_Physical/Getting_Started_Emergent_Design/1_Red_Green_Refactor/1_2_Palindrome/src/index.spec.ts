2497ce05ce66dc027205463114db23936e10c1a1
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-08 03:50:41 +0000

    test(red): create test cases to validate if phrase contains a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -18,4 +18,12 @@ describe("palindrome checker", () => {
   it("should return 'true' when 'Mom' is the input since the word is a palindrome and casing should be ignores", () => {
     expect(palindromeChecker.isPalindrome("Mom")).toBe(true);
   });
+
+  it("should return 'true' when 'Was It A Rat I Saw' is used as input since the pharse contains a palindrome", () => {
+    expect(palindromeChecker.isPalindrome("Was It A Rat I Saw")).toBe(true);
+  });
+
+  it("should return 'false' when 'Never Odd or Even' is used as input since the pharse do not contains a palindrome", () => {
+    expect(palindromeChecker.isPalindrome("Never Odd or Even")).toBe(false);
+  });
 });
