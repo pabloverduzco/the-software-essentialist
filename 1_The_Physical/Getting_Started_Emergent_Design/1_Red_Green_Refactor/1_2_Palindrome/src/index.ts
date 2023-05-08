0f26c6bc8ed383426cc1430d7e40bb6f175489f8
Pablo Verduzco <122489925+pabloverduzco@users.noreply.github.com>
2023-05-08 04:06:09 +0000

    refactor(green): transform isPalindrome method to check if a phrase contains a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,7 +1,15 @@
 export default class PalindromeChecker {
-  isPalindrome(word: string): boolean {
-    return (
-      word.split("").reverse().join("").toLowerCase() === word.toLowerCase()
-    );
+  private reverseWord(word: string): string {
+    return word.split("").reverse().join("");
+  }
+
+  public isPalindrome(pharse: string): boolean {
+    const words = pharse.split(" ");
+    for (const word of words) {
+      const reversedWord = this.reverseWord(word).toLowerCase();
+      if (reversedWord === word.toLowerCase()) return true;
+    }
+
+    return false;
   }
 }
