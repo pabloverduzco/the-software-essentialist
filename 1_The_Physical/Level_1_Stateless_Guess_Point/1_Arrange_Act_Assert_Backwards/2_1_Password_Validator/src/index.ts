276208141655fb54a6497c6343cdbe645d76cd24
Pablo Verduzco <pabloverduzcos@icloud.com>
2023-05-13 13:05:48 -0600

    test(green): create basic password validator structure

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -2,8 +2,8 @@ export class PasswordValidator {
 
     public validate(password: string): {isValid: boolean, errors: Array<string> | null} {
         return {
-            isValid: true,
-            errors: null
+            isValid: false,
+            errors: ["InvalidLength"]
         }
     }
 }
