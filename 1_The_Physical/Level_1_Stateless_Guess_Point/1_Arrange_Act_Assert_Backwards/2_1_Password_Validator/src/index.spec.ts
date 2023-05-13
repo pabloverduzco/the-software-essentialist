486456bf9ebfdfad6674defb71a63a6d01cb1309
Pablo Verduzco <pabloverduzcos@icloud.com>
2023-05-13 14:08:56 -0600

    test(refactor): create password validator instance before each test execution

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,10 +1,15 @@
 import { PasswordValidator } from "./index";
 
+let passwordValidator: PasswordValidator;
+
+beforeEach(() => {
+  passwordValidator = new PasswordValidator()
+})
+
 describe('password validator', () => {
-  test('should return invalid length error when "maxwell1_c" is used as password due a lack of uppercase characters', () => {
+  test('should return not uppercase characters error when "maxwell1_c" is used as password due a lack of uppercase characters', () => {
     const password = "maxwell1_c"
-    
-    const passwordValidator = new PasswordValidator();
+
     const result = passwordValidator.validate(password)
     
     expect(result.isValid).toBe(false)
@@ -12,10 +17,9 @@ describe('password validator', () => {
     expect(result.errors).toContain("NotUppercaseCharacters")
   })
 
-  test('should return invalid length error when "maxwellTheBe" is used as password because a lack of digits', () => {
+  test('should return not digits error when "maxwellTheBe" is used as password because a lack of digits', () => {
     const password = "maxwellTheBe"
     
-    const passwordValidator = new PasswordValidator();
     const result = passwordValidator.validate(password)
     
     expect(result.isValid).toBe(false)
@@ -23,10 +27,9 @@ describe('password validator', () => {
     expect(result.errors).toContain("NotDigits")
   })
 
-  test('should return invalid length error when "thePhysical1234567" is used as password', () => {
+  test('should return invalid length error when "thePhysical1234567" is used as password due the exceeding characters length', () => {
     const password = "thePhysical1234567"
     
-    const passwordValidator = new PasswordValidator();
     const result = passwordValidator.validate(password)
     
     expect(result.isValid).toBe(false)
