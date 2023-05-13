ea6376c256c8b1651eb9547ff193fe405cf77ac3
Pablo Verduzco <pabloverduzcos@icloud.com>
2023-05-13 13:51:32 -0600

    test(red): create test case to check if a password contains at least one digit

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,6 +12,17 @@ describe('password validator', () => {
     expect(result.errors).toContain("NotUppercaseCharacters")
   })
 
+  test('should return invalid length error when "maxwellTheBe" is used as password because a lack of digits', () => {
+    const password = "maxwellTheBe"
+    
+    const passwordValidator = new PasswordValidator();
+    const result = passwordValidator.validate(password)
+    
+    expect(result.isValid).toBe(false)
+    expect(result.errors).toHaveLength(1)
+    expect(result.errors).toContain("NotDigits")
+  })
+
   test('should return invalid length error when "thePhysical1234567" is used as password', () => {
     const password = "thePhysical1234567"
     
