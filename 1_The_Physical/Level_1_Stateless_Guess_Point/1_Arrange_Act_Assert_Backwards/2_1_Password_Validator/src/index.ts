188ca0dcbc1b1c647a884a463dc70ea97717bf56
Pablo Verduzco <pabloverduzcos@icloud.com>
2023-05-13 13:43:03 -0600

    test(green): improve password validator code to catch different rules depending the case

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,9 +1,29 @@
 export class PasswordValidator {
 
+    private rules = [
+        {
+            "regex": /[A-Z]/g,
+            "message": "NotUppercaseCharacters",
+        },
+        {
+            "regex": /^.{5,15}$/g,
+            "message": "InvalidLength"
+        }
+    ]
+
     public validate(password: string): {isValid: boolean, errors: Array<string> | null} {
+        const errors = []
+
+        for (let i = 0; i < this.rules.length; i++) {
+
+            if (password.match(this.rules[i].regex)) continue;
+
+            errors.push(this.rules[i].message)
+        } 
+        
         return {
-            isValid: false,
-            errors: ["InvalidLength"]
+            isValid: errors.length == 0,
+            errors: errors
         }
     }
 }
